38aa24ce48c4fa43ca4434b42774a605a916f951
umtozl <65434292+umtozl@users.noreply.github.com>
2021-01-02 17:30:11 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,55 @@
+let zar = 0
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 8; index++) {
+        zar = randint(1, 6)
+    }
+    if (zar == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (zar == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (zar == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (zar == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (zar == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (zar == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
+})
